e540d19a36c5d71e0a5b3356cb7002c0b3beb065
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2023-12-13 15:54:08 -0500

    -- sidebar fix

--- js/layout.js
+++ js/layout.js
@@ -1,27 +1,51 @@
 $( document ).ready(function() {
   /* Toggle sidebar */
   let sidebarIsExpanded = false;
-  const sidebarCollapsedWidth = '128px';
-  const sidebarExpandedWidth = '256px';
+  let sidebarCollapsedWidth;
+  let sidebarExpandedWidth;
 
   sidebarIsExpanded ? collapseSidebar() : expandSidebar();
-  
+
+  jQuery(window).resize(function(){
+    checkWidth();
+    resizeSidebar();
+  });
+
   jQuery("#toggleSidebarButton").on( "click", function() {
     sidebarIsExpanded ? collapseSidebar() : expandSidebar();
   });
 
+  function checkWidth(){
+    const windowsize = jQuery(window).width();
+    if (windowsize > 576 ) { //sm breakpoint
+      sidebarCollapsedWidth = '128px';
+      sidebarExpandedWidth = '256px';
+    } else {
+      sidebarCollapsedWidth = '64px';
+      sidebarExpandedWidth = '140px';
+    }
+  }
+
+  function resizeSidebar(){
+    !sidebarIsExpanded ? collapseSidebar() : expandSidebar();
+  }
+
   function collapseSidebar(){
+    checkWidth();
     jQuery("#sidebar-menu").removeClass('align-items-stretch');
     jQuery("#sidebar").width(sidebarCollapsedWidth);
     jQuery(".sidebar-text").hide();
+    //jQuery("header").css("margin-left", sidebarCollapsedWidth);
     jQuery("#main").css("margin-left", sidebarCollapsedWidth);
     sidebarIsExpanded = false;
   }
 
   function expandSidebar(){
+    checkWidth();
     jQuery("#sidebar-menu").addClass('align-items-stretch');
     jQuery("#sidebar").width(sidebarExpandedWidth);
     jQuery(".sidebar-text").show();
+    //jQuery("#header").css("margin-left", sidebarExpandedWidth);
     jQuery("#main").css("margin-left", sidebarExpandedWidth);
     sidebarIsExpanded = true;
   }
